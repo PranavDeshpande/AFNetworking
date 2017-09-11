749d8b27b7c8ac230f404a9821d0369506827add
Pranav Deshpande <pranavdeshpande12592@gmail.com>
2017-09-11 09:44:44 -0700

    first commit

--- AFNetwork/ViewController.swift
+++ AFNetwork/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-AFNetwork/Pods-AFNetwork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFNetwork : NSObject
+@end
+@implementation PodsDummy_Pods_AFNetwork
+@end
